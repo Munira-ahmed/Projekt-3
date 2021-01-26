af520297190155f3514b31edf436fd0051dd5a07
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-01-26 14:52:50 +0100

    todo-list, test java

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function Addnumbers(a,b) {
+    return a+b;
+}
+
+let mybutton = document.querySelector(".mybutton");
+//console.log(typeof mybutton)
+
+
+let ClickofAButton = mybutton.addEventListener("click", () => {
+    console.log("idk!");
+});
+
+if (ClickofAButton == true) {
+    console.log("No")
+}
+
