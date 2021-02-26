5482b164d26566109cc65b86473b00dbad85b0b7
Mona Lisa <munira.ahmed@elev.ga.ntig.se>
2021-02-26 18:53:19 +0100

    lyckades inte med localStorage, enkel todos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const TODOS = "main.todos";
-let todos = JSON.parse(localStorage.getItem(TODOS)) || [];
+
 //hämta olika html element 
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
@@ -7,7 +6,7 @@ const todoList = document.querySelector(".todo-list");
 
 
 //eventlisteners - gör saker beroende på ett event, tex en klick
-document.addEventListener("DOMContentLoaded", showTodos);// ska visa todo även fast sidan har updaterats
+
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteTodo);
 
@@ -27,8 +26,7 @@ function addTodo(event) {
    newTodo.innerText = todoInput.value;
    newTodo.classList.add('todo-item');
    todoDiv.appendChild(newTodo);
-   //lägg todoinput.value till savetodo och pusha den in i todos
-   saveTodos(todoInput.value);
+
    //ta bort  knapp
    const deleteButton = document.createElement('button');
    deleteButton.innerText ='x';
@@ -52,31 +50,3 @@ function deleteTodo(e){
     }  
    
 }
-
-function saveTodos(todo) {
-
-    todos.push(todo);
-    localStorage.setItem(TODOS, JSON.stringify(todos));
-}
-
-//man ska kunna se todos på documentet om det är i arrayen
-function showTodos(){
-    todos.forEach(function(todo) { //nästan hela addtodo
-    //Todo DIV
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-   //skapa LI
-   const newTodo = document.createElement('li');
-   newTodo.innerText = todo;
-   newTodo.classList.add('todo-item');
-   todoDiv.appendChild(newTodo);
-   //ta bort  knapp
-   const deleteButton = document.createElement('button');
-   deleteButton.innerText ='x';
-   deleteButton.classList.add("delete-btn");
-   todoDiv.appendChild(deleteButton);
-   //append to list
-   todoList.appendChild(todoDiv);
-  });
-}
-
